c006cc45b557a204112b7d2792ae9db45b2d1802
bradsen <38464644+bradsen@users.noreply.github.com>
2025-01-01 11:37:07 +0800

    git commit 'message'

--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -5,7 +5,20 @@ header ("X-XSS-Protection: 0");
 // Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Feedback for end user
+	// Get input
+	$name = htmlspecialchars( $_GET[ 'name' ] );
+
+	$html .= "<pre>Hello {name}</pre>";
+}
+
+?>
+
+/*
+// Is there any input?
+if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Feedback for end user
 	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
 }
 
 ?>
+*/
