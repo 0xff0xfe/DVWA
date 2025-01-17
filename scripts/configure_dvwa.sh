fc8a4abe135a25157d165bc51b7eae04b93c7681
0xff0xfe <145196769+0xff0xfe@users.noreply.github.com>
2025-01-17 17:31:57 +0800

    Update configure_dvwa.sh

--- scripts/configure_dvwa.sh
+++ scripts/configure_dvwa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set up DVWA
-cd /var/www/dvwa/config
+cd /var/www/html/config
 cp config.inc.php.dist config.inc.php
 # Set up database (create dvwa database, etc.)
 mysql -u root -e "CREATE DATABASE dvwa;"
