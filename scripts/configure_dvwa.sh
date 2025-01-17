5b63f1e9ae0241de2ba213618cceb41c41563ebc
0xff0xfe <145196769+0xff0xfe@users.noreply.github.com>
2025-01-17 18:26:02 +0800

    Update configure_dvwa.sh

--- scripts/configure_dvwa.sh
+++ scripts/configure_dvwa.sh
@@ -3,7 +3,7 @@
 cd /var/www/html/config
 cp config.inc.php.dist config.inc.php
 # Set up database (create dvwa database, etc.)
-mysql -u root -e "CREATE DATABASE dvwa;"
+sudo mysql -u root -e "CREATE DATABASE dvwa;"
 CREATE USER 'dvwausr'@'127.0.0.1' IDENTIFIED BY "dvwar@123";
 GRANT ALL PRIVILEGES ON dvwa.* TO 'dvwausr'@'localhost' IDENTIFIED BY 'dvwa@123';
 cd /var/www/dvwa
