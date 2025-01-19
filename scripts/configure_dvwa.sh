d14a9bdcbcf84beac7aff562a3c80df5323b47f1
0xff0xfe <145196769+0xff0xfe@users.noreply.github.com>
2025-01-19 09:35:55 +0800

    Update configure_dvwa.sh

--- scripts/configure_dvwa.sh
+++ scripts/configure_dvwa.sh
@@ -4,10 +4,10 @@ cd /var/www/html/config
 cp config.inc.php.dist config.inc.php
 # Set up database (create dvwa database, etc.)
 sudo mysql -u root -e "
-CREATE DATABASE dvwa;
+CREATE DATABASE IF NOT EXISTS dvwa;
 CREATE USER 'dvwausr'@'127.0.0.1' IDENTIFIED BY 'dvwar@123';
 GRANT ALL PRIVILEGES ON dvwa.* TO 'dvwausr'@'127.0.0.1';
 FLUSH PRIVILEGES;
 "
 cd /var/www/dvwa
-sudo chmod -R 777 /var/www/html/dvwa/hackable/uploads/
+sudo chmod -R 777 /var/www/html/hackable/uploads/
